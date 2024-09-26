c712ac73bc7794e0d03dd32868568621faa015e3
Revanth M <130986102+revanth-zessta@users.noreply.github.com>
2024-09-26 14:33:15 +0530

    First check-in

---
+++ signalrtc-backend-dotnet/Storage/RavenDBStore.cs
@@ -0,0 +1,24 @@
+﻿using Raven.Client.Documents;
+
+namespace SignalRtc.Storage
+{
+    public static class RavenDbStore
+    {
+        private static IDocumentStore store;
+
+        public static IDocumentStore GetStore()
+        {
+            if (store == null)
+            {
+                store = new DocumentStore
+                {
+                    Urls = new[] { "http://localhost:8080" }, // Your RavenDB URL
+                    Database = "ChatAppDb" // Name of your database
+                }.Initialize();
+            }
+
+            return store;
+        }
+    }
+
+}
